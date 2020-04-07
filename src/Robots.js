fe5f61c882f3767ee15bc47b20266235021d8a56
unknown <maggie.aytac@gmail.com>
2020-04-06 18:03:29 -0700

    here goes

---
+++ src/Robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+{
+	id: 1,
+	name: 'person 1',
+	username: 'person1',
+	email: 'person1@gmail.com'
+},
+{
+	id: 2,
+	name: 'person 2',
+	username: 'person2',
+	email: 'person2@gmail.com'
+},
+{
+	id: 3,
+	name: 'person 3',
+	username: 'person3',
+	email: 'person3@gmail.com'
+},
+{
+	id: 4,
+	name: 'person 4',
+	username: 'person4',
+	email: 'person4@gmail.com'
+},
+{
+	id: 5,
+	name: 'person 5',
+	username: 'person5',
+	email: 'person5@gmail.com'
+},
+]
